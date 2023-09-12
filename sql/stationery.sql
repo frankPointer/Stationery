171369a36d4ed94260ea47cc303949e18bda2c85
frankPointer <1256623959@qq.com>
2023-09-12 22:26:10 +0800

    完结

--- sql/stationery.sql
+++ sql/stationery.sql
@@ -15,28 +15,6 @@ CREATE TABLE Supplier
     ContactNumber VARCHAR(20)  NOT NULL, -- 联系电话
     Address       VARCHAR(100) NOT NULL  -- 地址
 );
-
--- 创建客户表
-CREATE TABLE Customer
-(
-    CustomerID    INT PRIMARY KEY,       -- 客户ID
-    CustomerName  VARCHAR(50)  NOT NULL, -- 客户名称
-    ContactNumber VARCHAR(20)  NOT NULL, -- 联系电话
-    Address       VARCHAR(100) NOT NULL  -- 地址
-);
-
--- 创建订单表
-CREATE TABLE Orders
-(
-    OrderID      INT PRIMARY KEY,         -- 订单ID
-    CustomerID   INT            NOT NULL, -- 客户ID，关联到客户表中的客户ID
-    OrderDate    DATE           NOT NULL, -- 订单日期
-    OrderStatus  VARCHAR(20)    NOT NULL, -- 订单状态
-    DeliveryDate DATE,                    -- 发货日期
-    TotalAmount  DECIMAL(10, 2) NOT NULL, -- 总金额
-    FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID)
-);
-
 -- 创建商品表
 CREATE TABLE Product
 (
@@ -51,15 +29,37 @@ CREATE TABLE Product
     FOREIGN KEY (SupplierID) REFERENCES Supplier (SupplierID)
 );
 
--- 创建订单明细表
-CREATE TABLE OrdersDetail
-(
-    OrderDetailID INT PRIMARY KEY,         -- 订单明细ID
-    OrderID       INT            NOT NULL, -- 订单ID，关联到订单表中的订单ID
-    ProductID     INT            NOT NULL, -- 商品ID，关联到文具商品表中的商品ID
-    Quantity      INT            NOT NULL, -- 数量
-    UnitPrice     DECIMAL(10, 2) NOT NULL, -- 单价
-    TotalAmount   DECIMAL(10, 2) NOT NULL, -- 总金额
-    FOREIGN KEY (OrderID) REFERENCES Orders (OrderID),
-    FOREIGN KEY (ProductID) REFERENCES product (ProductID)
-);
+-- 表的视图
+CREATE
+ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `category_view` AS
+select `category`.`id` AS `编号`, `category`.`CategoryName` AS `类别名称`, `category`.`Description` AS `分类描述`
+from `category`;
+CREATE
+ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `employee_view` AS
+select `employee`.`id`       AS `编号`,
+       `employee`.`name`     AS `名字`,
+       `employee`.`gender`   AS `性别`,
+       `employee`.`password` AS `密码`,
+       `employee`.`phone`    AS `手机号码`
+from `employee`;
+CREATE
+ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `product_view` AS
+select `p`.`id`            AS `商品ID`,
+       `p`.`ProductName`   AS `商品名称`,
+       `c`.`CategoryName`  AS `类别名称`,
+       `s`.`SupplierName`  AS `供应商名称`,
+       `p`.`Price`         AS `价格`,
+       `p`.`StockQuantity` AS `库存数量`,
+       `p`.`Description`   AS `描述`
+from ((`product` `p` join `category` `c` on ((`p`.`CategoryID` = `c`.`id`))) join `supplier` `s`
+      on ((`p`.`SupplierID` = `s`.`id`)));
+CREATE
+ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `supplier_view` AS
+select `supplier`.`id`            AS `编号`,
+       `supplier`.`SupplierName`  AS `名字`,
+       `supplier`.`ContactPerson` AS `联系人`,
+       `supplier`.`ContactNumber` AS `联系电话`,
+       `supplier`.`Address`       AS `地址`
+from `supplier`;
+
+-- 数据库自己写
